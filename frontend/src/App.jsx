b914b67e483708a2154d29f1edbadd64ec14538d
dkq <82295670+dkqdkq@users.noreply.github.com>
2025-01-26 18:18:03 +0900

    session 통신 테스트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import axios from 'axios';
+import Session from 'react-session-api';
 
 function App() {
     return (
         <>
-            <h1>hello, react!</h1>
+            <button
+                onClick={() => {
+                    axios.get('/api/test').then((data) => {
+                        console.log(Session.items());
+                        console.log(data);
+                        console.log(document.cookie);
+                    });
+                }}
+            >
+                <h1>hello, react!</h1>
+            </button>
         </>
     );
 }
