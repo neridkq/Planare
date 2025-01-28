873c85d6adf50b3e367641d8fd6a03c58d185b43
dkq <dev.dkq@gmail.com>
2025-01-28 19:49:03 +0900

    단순히 "작동"만 하는 기능 완성, 코드 보완 필요

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -6,21 +6,63 @@ import axios from 'axios';
 import Session from 'react-session-api';
 
 function App() {
-    return (
-        <>
-            <button
-                onClick={() => {
-                    axios.get('/api/test').then((data) => {
-                        console.log(Session.items());
-                        console.log(data);
-                        console.log(document.cookie);
-                    });
-                }}
-            >
-                <h1>hello, react!</h1>
-            </button>
-        </>
-    );
+    const [loggedIn, setLoggedIn] = useState(null);
+    const [userInfo, setUserInfo] = useState(null);
+    const [loginUrl, setLoginUrl] = useState(null);
+
+    useEffect(() => {
+        axios.get('/api/get_user_info').then((res) => {
+            if (res.data.status === 401) {
+                console.log(res.data.data.url);
+                setLoggedIn(false);
+                setUserInfo(null);
+                setLoginUrl(res.data.data.url);
+            } else if (res.data.status === 200) {
+                console.log(res.data.data);
+                setLoggedIn(true);
+                setUserInfo(res.data.data);
+            }
+        });
+    }, [loginUrl]);
+    if (loggedIn) {
+        return (
+            <>
+                <div>userInfo.id: {userInfo.id}</div>
+                <div>userInfo.connected_at: {userInfo.connected_at}</div>
+                <div>userInfo.properties.nickname: {userInfo.properties.nickname}</div>
+                <div>
+                    userInfo.properties.profile_image(50x50):
+                    <img width='50' height='50' src={userInfo.properties.profile_image}></img>
+                </div>
+                <div>
+                    userInfo.properties.thumbnail_image:
+                    <img src={userInfo.properties.thumbnail_image}></img>
+                </div>
+                <button
+                    onClick={() => {
+                        axios.get('/api/logout').then((res) => {
+                            setLoggedIn(false);
+                            alert(res.data.data.msg);
+                        });
+                    }}
+                >
+                    log아웃하기
+                </button>
+            </>
+        );
+    } else if (loggedIn === false) {
+        return (
+            <>
+                <button
+                    onClick={() => {
+                        window.location.href = loginUrl;
+                    }}
+                >
+                    Login하기
+                </button>
+            </>
+        );
+    }
 }
 
 export default App;
